d98838f394afe68b8b3d61205d3284263e7b37e7
dwyt-dz <dwight.zambrano@gmail.com>
2020-10-26 21:14:27 +0800

    update

--- front-page.php
+++ front-page.php
@@ -33,58 +33,8 @@
 			</div>
 		</div>
 	</section>
-	<section class="pmtnbar-dzy">
-		<div class="container content-wrap">
-			<div class="pmtn-imgs">
-				<?php if( have_rows('advertisements', 'option') ): ?>
-				    <?php while( have_rows('advertisements', 'option') ): the_row(); 
-				    	$link = get_sub_field('link');
-					    $link_url = $link['url'];
-					    $link_title = $link['title'];
-					    $link_target = $link['target'] ? $link['target'] : '_self';
-					    $image = get_sub_field('image');
-				        $size = 'large';
-				        ?>
-						<a class="pmtn-link" href="<?php echo esc_url( $link_url ); ?>" target="<?php echo esc_attr( $link_target ); ?>">
-							<?php echo wp_get_attachment_image( $image, $size ); ?>
-				        </a>
-				    <?php endwhile; ?>
-				<?php endif; ?>
-			</div>
-		</div>
-	</section>
 	
-<?php
-
-	// Check value exists.
-	if( have_rows('section_display') ):
-	    // Loop through rows.
-	    while ( have_rows('section_display') ) : the_row();
-
-	        // Case: Paragraph layout.
-	        if( get_row_layout() == 'featured_slider' ):
-	            get_template_part('layouts/section', 'featslider');
-	            // Do something...
-
-	        elseif( get_row_layout() == 'featured_posts' ):
-	            get_template_part('layouts/section', 'featposts');
-
-	        elseif( get_row_layout() == 'featured_tab_display' ):
-	            get_template_part('layouts/section', 'tabdisplay'); 
-
-	        elseif( get_row_layout() == 'card_display' ):
-	            get_template_part('layouts/section', 'carddisplay');    
-	        endif;
-
-	    // End loop.
-	    endwhile;
-
-	// No value.
-	else :
-	    // Do something...
-	endif;
-
-	?>
+	<?php get_template_part('../snippet-templates'); ?>
 
 	</main>
 <?php get_footer(); ?>
---
+++ snippet-templates.php
@@ -0,0 +1,59 @@
+<?php 
+/**
+*
+*/
+get_header(); 
+
+$subtitle 	= get_field('page_subtitle');
+$hidetitle  = get_field('hide_title');
+?>
+
+<main>
+	<?php if($hidesc == "NO") : ?>
+	<header class="page-sc container-dzy">
+		<h1><?php the_title(); ?></h1>
+		<?php if($subtitle) : ?>
+			<p class='subtitle'><?php echo $subtitle; ?></p>
+		<?php endif; ?>
+	</header>
+	<?php endif; ?>
+
+	<?php
+
+	// Check value exists.
+	if( have_rows('shop_display') ):
+	    // Loop through rows.
+	    while ( have_rows('shop_display') ) : the_row();
+
+	        // Case: Paragraph layout.
+	        if( get_row_layout() == 'display_collection' ):
+	            get_template_part('layouts/shop', 'discollection');
+	            // Do something...
+
+	        elseif( get_row_layout() == 'featured_banner' ):
+	            get_template_part('layouts/shop', 'featbanner');
+
+	        elseif( get_row_layout() == 'display_products' ):
+	            get_template_part('layouts/shop', 'disproducts');
+
+			elseif( get_row_layout() == 'page_banner' ):
+	            get_template_part('layouts/shop', 'pagebanner');
+
+	        elseif( get_row_layout() == 'ad_spots' ):
+	            get_template_part('layouts/ad', 'spots');    
+
+	          
+	        endif;
+
+	    // End loop.
+	    endwhile;
+
+	// No value.
+	else :
+		the_content();
+	endif;
+
+	?>
+</main>
+
+<?php get_footer(); ?>
