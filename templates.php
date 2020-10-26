73d0085936df76c5efdd91d99541e846044149cd
dwyt-dz <dwight.zambrano@gmail.com>
2020-10-26 21:38:38 +0800

    Update templates.php

--- templates.php
+++ templates.php
@@ -17,10 +17,11 @@ if( have_rows('section_display') ):
 
         elseif( get_row_layout() == 'card_display' ):
             get_template_part('layouts/section', 'carddisplay');    
-        endif;
 
         elseif( get_row_layout() == 'ad_spots' ):
             get_template_part('layouts/ad', 'spots');    
+
+        endif;
 
     // End loop.
     endwhile;
@@ -30,5 +31,38 @@ else :
     // Do something...
 endif;
 
+// Check value exists.
+
+if( have_rows('shop_display') ):
+    // Loop through rows.
+    while ( have_rows('shop_display') ) : the_row();
+
+        // Case: Paragraph layout.
+        if( get_row_layout() == 'display_collection' ):
+            get_template_part('layouts/shop', 'discollection');
+            // Do something...
+
+        elseif( get_row_layout() == 'featured_banner' ):
+            get_template_part('layouts/shop', 'featbanner');
+
+        elseif( get_row_layout() == 'display_products' ):
+            get_template_part('layouts/shop', 'disproducts');
+
+		elseif( get_row_layout() == 'page_banner' ):
+            get_template_part('layouts/shop', 'pagebanner');
+
+        elseif( get_row_layout() == 'ad_spots' ):
+            get_template_part('layouts/ad', 'spots');    
+
+          
+        endif;
+
+    // End loop.
+    endwhile;
+
+// No value.
+else :
+	the_content();
+endif;
 
 ?>
