95ca254b66714273e031fde590d4e572e6fd8e3c
dwyt-dz <dwight.zambrano@gmail.com>
2020-10-26 19:32:08 +0800

    fix

--- header.php
+++ header.php
@@ -9,33 +9,14 @@
 <body <?php body_class(); ?>>
 
 	<header class="header-sc">
-		<div class="topbar-sc container">
-			<?php 
-				$image = get_field('website_logo', 'option');
-
-					if( !empty( $image ) ): ?>
-					    <img class="logo" src="<?php echo esc_url($image['url']); ?>" alt="<?php echo esc_attr($image['alt']); ?>" />
-					<?php endif; ?>
-			<div class="container-dzy flex-nowrap">
-				<?php //get_template_part('includes/snippet', 'loginAccount');?>
-				<?php //get_template_part('includes/section', 'loginForm');?>
-				<div class="social-wrap">
-					<?php //get_template_part('includes/snippet', 'socials');?>
-				</div>
-				<!-- use gradient bar - orange violet orange -->
-			</div>
-		</div>
-		<div class="main-nav-sc">
-			<div class="container">
-				
-				<?php
-					wp_nav_menu( array( 
-					    'theme_location' 	=> 	'dz-action-main-menu',
-					    'orderby' 			=>	'menu_order',
-					    'container' 	=> 	'ul' ) ); 
-					?>
-				<!-- use plain bar..black.. or switch colors with the topbar -->
-				<span class="inner-nav nav-btn hidden"><i class="fa fa-bars"></i></span>
-			</div>
+		<div class="container">
+			<?php
+				wp_nav_menu( array( 
+				    'theme_location' 	=> 	'dz-action-main-menu',
+				    'orderby' 			=>	'menu_order',
+				    'container' 	=> 	'ul' ) ); 
+				?>
+			<!-- use plain bar..black.. or switch colors with the topbar -->
+			<span class="inner-nav nav-btn hidden"><i class="fa fa-bars"></i></span>
 		</div>
 	</header>
